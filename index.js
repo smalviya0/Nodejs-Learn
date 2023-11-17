e893cd1f2f60e0c7ba56492f3c6a9a1bd587a934
Shailendra Malviya <shailendra.malviya@preciousinfosystem.com>
2023-11-17 23:41:03 +0530

    filterdata

---
+++ index.js
@@ -0,0 +1,20 @@
+// var a = 10
+// var b =20
+// var c =20
+// console.log(a+b+c)
+// var x = '20';
+// const arr = [2, 4,7, 1,3, 8, 3]
+// console.log(arr[0])
+
+//module import
+// const app = require("./app")
+// const arr = [2, 4,7, 1,3, 8, 3]
+// // console.log(app.y)
+
+//Filter function
+const arr = [2, 4, 7, 1,3, 8, 3]
+let result = arr.filter((item)=>{
+    return item>4
+})
+console.warn(result)
+
