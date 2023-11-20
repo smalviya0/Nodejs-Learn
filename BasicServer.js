35a1bc0b01527043b528fa08ba3cc88e5911f5ab
Shailendra Malviya <shailendra.malviya@preciousinfosystem.com>
2023-11-20 22:52:08 +0530

    arrorfunction

--- BasicServer.js
+++ BasicServer.js
@@ -1,6 +1,33 @@
 //http - handle server request and response
+// const http = require('http');
+
+// http.createServer((req, resp)=>{
+//     resp.write("<h1>Hello this is dummy data</h1>")
+//     resp.end();
+// }).listen(4500);
+//pass using function
+
+// const http = require('http');
+
+// function dataCount(req, resp){
+//     resp.write("<h1>Hii this is form data</h1>")
+//     resp.end();
+// }
+// http.createServer(dataCount).listen(4500);
+//using arrow function
+// const http = require('http');
+
+// const dataCount = (req, resp) =>{
+//     resp.write("<h1>Hello guys this is form data</h1>")
+//     resp.end();
+// }
+// http.createServer(dataCount).listen(4500);
+
+//pass as a parameter
 const http = require('http');
-http.createServer((req, resp)=>{
-    resp.write("Hello this is dummy data")
+
+http.createServer((req, resp) =>{
+    resp.write("<h1>Hello this is form data</h1>")
     resp.end();
-}).listen(4500);
+}).listen(4500)
+
--- coremodule.js
+++ coremodule.js
@@ -10,3 +10,11 @@ console.log("file name",__filename) // file name
 
 const gs = require('fs').writeFileSync;
 gs("xyz.txt", "abc")
+
+const http = require('http');
+
+function dataCount(req, resp){
+    resp.write("<h1>Hello this is form data</h1>")
+    resp.end();
+}
+http.createServer(dataCount).listen(4500);
